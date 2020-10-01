6b548d0fa088992c3ea65931d4fb18a39cadffdf
EdithMa <maturranoedith@gmail.com>
2020-09-30 19:14:50 -0500

    creando carpetas

---
+++ BD/db_matriculas.sql
@@ -0,0 +1,4 @@
+SELECT * FROM sistem_matriculas.usuario;
+
+INSERT INTO `sistem_matriculas`.`usuario` (`apellido`, `email`, `nombre`) VALUES ('Maturrano Roman', 'algo@gmail.com', 'Edith');
+INSERT INTO `sistem_matriculas`.`usuario` (`apellido`, `email`, `nombre`) VALUES ('Lopez', 'correo@gmail.com', 'Lindsay');
