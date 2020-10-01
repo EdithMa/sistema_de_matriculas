b57831c6370c9f56f8776b19ba0d832b9938ee3a
EdithMa <maturranoedith@gmail.com>
2020-09-30 20:30:44 -0500

    segundo commit

--- BD/db_matriculas.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM sistem_matriculas.usuario;
-
-INSERT INTO `sistem_matriculas`.`usuario` (`apellido`, `email`, `nombre`) VALUES ('Maturrano Roman', 'algo@gmail.com', 'Edith');
-INSERT INTO `sistem_matriculas`.`usuario` (`apellido`, `email`, `nombre`) VALUES ('Lopez', 'correo@gmail.com', 'Lindsay');
